3bf917231aec520fe18fd57f0467377a885da4a0
Shivam Agrawal <shivam19.aggrawal@gmail.com>
2026-06-12 16:16:14 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,20 @@
 from app import create_app, db
 from app.models import User
+import os
 
 app = create_app()
-
 
 @app.shell_context_processor
 def _shell_ctx():
     return {"db": db, "User": User}
 
+def seed_if_needed():
+    if os.environ.get("SEED_ON_STARTUP") == "1":
+        with app.app_context():
+            from app.seed import seed_data
+            seed_data()
+
+seed_if_needed()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
